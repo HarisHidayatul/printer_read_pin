ad2001759afb6aa527a66824f651c4ae5f76cdbb
unknown <hidayatullohharis@gmail.com>
2025-03-07 11:37:07 +0700

    .

--- python/process_to_coordinate.py
+++ python/process_to_coordinate.py
@@ -75,42 +75,42 @@ def main():
             string_result_data = string_result_data + '\n' #+ '  ' + str(timer_selisih)
             # print()
     # print(string_result_data)
-    filter_downsample_character = downsample_character(string_result_data)
-    print(downsample_character(string_result_data))
-    # enter_detect = False
-    # data_detect = False
-    # temp_data = []
-    # string_data = []
-    # for loop_row in filter_downsample_character.split("\n"):
-    #     if enter_detect:
-    #         if loop_row.strip() == "":
-    #             if data_detect:
-    #                 # break
-    #                 temp_data.append(string_data)
-    #                 enter_detect = False
-    #                 data_detect = False
-    #                 string_data = []
-    #             else:
-    #                 continue
-    #         else:
-    #             data_detect = True
-    #             string_data.append(loop_row)
-    #     else:
-    #         if loop_row.strip() == "":
-    #             # print("Enter")
-    #             enter_detect = True
-    # for loop_data in temp_data:
-    #     # List untuk menyimpan koordinat (x, y)
-    #     coordinates = []
+    filter_downsample_character = "\n\n" + downsample_character(string_result_data)
+    print(filter_downsample_character)
+    enter_detect = False
+    data_detect = False
+    temp_data = []
+    string_data = []
+    for loop_row in filter_downsample_character.split("\n"):
+        if enter_detect:
+            if loop_row.strip() == "":
+                if data_detect:
+                    # break
+                    temp_data.append(string_data)
+                    enter_detect = False
+                    data_detect = False
+                    string_data = []
+                else:
+                    continue
+            else:
+                data_detect = True
+                string_data.append(loop_row)
+        else:
+            if loop_row.strip() == "":
+                # print("Enter")
+                enter_detect = True
+    for loop_data in temp_data:
+        # List untuk menyimpan koordinat (x, y)
+        coordinates = []
 
-    #     # Baris terbawah menjadi y = 0
-    #     max_y = len(loop_data) - 1
+        # Baris terbawah menjadi y = 0
+        max_y = len(loop_data) - 1
 
-    #     for y, row in enumerate(reversed(loop_data)):  # Balik agar baris terbawah jadi y = 0
-    #         for x, char in enumerate(row):
-    #             if char == 'X':
-    #                 coordinates.append([x, y])  # Simpan koordinat (x, y)
-    #     printing.printing_byte(coordinates)
+        for y, row in enumerate(reversed(loop_data)):  # Balik agar baris terbawah jadi y = 0
+            for x, char in enumerate(row):
+                if char == 'X':
+                    coordinates.append([x, y])  # Simpan koordinat (x, y)
+        printing.printing_byte(coordinates)
 
 
 if __name__ == "__main__":
